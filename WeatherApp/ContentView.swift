8292e84fd5c04c420655cedcf457da6c81ac9eee
Gurpreet <gurpreet.singh@elysiumlabs.in>
2024-05-28 10:14:19 +0530

    Initial Commit

---
+++ WeatherApp/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by NewsBytes on 27/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: [.blue,.white]),
+                        startPoint: .topLeading,
+                        endPoint: .bottomTrailing)
+                    .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Text("Cupertino, CA")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                
+                VStack(spacing: 8) {
+                    Image(systemName: "sun.max.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    
+                    Text("76°")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundStyle(.white)
+                }
+                    
+                Spacer()
+                    
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by NewsBytes on 27/05/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
